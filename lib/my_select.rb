a05f0c01cec5333035d1f7f529e8b9529606b14e
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:47:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+ i = 0
+ narray = []
+if array.size == 0
+  return nil
+else
+  while i < array.size
+    narray << array[i] if yield(array[i]) == true
+    i += 1
+  end
+end
+narray
 end
