d16e9a68103cb862a779fdca0b0af411362daaf4
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:46:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,7 +6,7 @@ if array.size == 0
   return nil
 else
   while i < array.size
-    narray << yield(array[i]) if block.call(array[i]) == true
+    narray << yield(array[i]) if yield(array[i]) == true
     i += 1
   end
 end
